1052abd3eac846dcb00a7f78d6f5705bee8e9ac2
David Pardo <david.pardo@rappi.com>
2024-04-20 13:21:24 -0500

    End Trainig

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
         'views/estate_property_views.xml',
         'views/estate_property_tags_views.xml',
         'views/estate_propery_offer.xml',
+        'views/res_users_views.xml',
         'views/estate_menus.xml',
     ],
     "application": True,
